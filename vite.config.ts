4d7f4116fb91aa2113692d81621ea3070aba20d3
Shivank Kacker <kacker.shivank@gmail.com>
2025-04-06 22:25:09 +0530

    added google analytics id

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePluginRadar({
+      analytics: {
+        id: "G-5EE9JSJFPR",
+      },
+    }),
+  ],
+});
